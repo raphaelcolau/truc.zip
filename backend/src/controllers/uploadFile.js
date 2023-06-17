4204cbe393d463b8b35b70c30753a54523c2a4ad
Raphael C <polyster20@gmail.com>
2023-06-17 14:33:14 +0200

    Update uploadFile.js
    wip

--- backend/src/controllers/uploadFile.js
+++ backend/src/controllers/uploadFile.js
@@ -2,5 +2,12 @@ const { default: axios } = require('axios');
 const File = require('../models/files.model.js');
 
 exports.uploadFile = async (req, res) => {
-    res.send("upload");
+    const file = req.files.file;
+
+    if (!file) res.status(400).send("No file uploaded");
+
+    res.status(200).send({
+        fileName: file.name,
+    });
+
 }
