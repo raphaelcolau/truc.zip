21cf0987aaf91d633542ac6628f531681379e4e8
Raphael C <polyster20@gmail.com>
2023-06-17 15:25:10 +0200

    Update files.route.js

--- backend/src/routes/files.route.js
+++ backend/src/routes/files.route.js
@@ -5,7 +5,7 @@ const { getFile } = require('../controllers/getFile');
 const { uploadFile } = require('../controllers/uploadFile');
 
 router.get("/:id", getFile); // GET a file by id
-router.post("/:id", uploadFile) // POST a file
+router.post("/", uploadFile) // POST a file
 router.delete("/:id", deleteFile) // DELETE a file by id
 
 module.exports = router;
