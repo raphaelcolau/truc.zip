00c3cbfe3924a5ae6a7685cdc7fc792ee43c410e
Raphael C <polyster20@gmail.com>
2023-06-17 14:49:52 +0200

    Update uploadFile.js
    upload function done

--- backend/src/controllers/uploadFile.js
+++ backend/src/controllers/uploadFile.js
@@ -1,13 +1,42 @@
 const { default: axios } = require('axios');
 const File = require('../models/files.model.js');
+const fs = require('fs');
 
 exports.uploadFile = async (req, res) => {
-    const file = req.files.file;
+    const file = req.files.files;
 
-    if (!file) res.status(400).send("No file uploaded");
+    if (!file) {
+        return res.status(400).send('No files were uploaded.');
+    };
 
-    res.status(200).send({
-        fileName: file.name,
-    });
+    try {
+        const randomId = Math.random().toString(36).substring(2, 8);
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    
+        const fileData = new File({
+            name: file.name,
+            url: randomId,
+            type: file.mimetype,
+            size: file.size,
+            md5: file.md5,
+            created_by: ip,
+        });
+        await fileData.save();
+
+        fs.mkdirSync('./files/' + randomId, { recursive: true }, (err) => {
+            if (err) throw err;
+        });
+        fs.writeFileSync('./files/' + randomId + '/' + file.name, file.data, (err) => {
+            if (err) throw err;
+        });
+        
+        res.status(200).send({
+            url: randomId,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Error while uploading file");
+    }
 
 }
