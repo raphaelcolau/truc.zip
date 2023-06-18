40920cc9b35a1b27825733c8c76f4d52558a6db7
Raphael C <polyster20@gmail.com>
2023-06-18 13:23:37 +0200

    refactoring

---
+++ frontend/src/components/button/ButtonBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function ButtonBar() {
+    return (
+      <div className="button-bar">
+        <a href="https://raphael.colau.fr" target="_blank" rel="noreferrer">
+          <button className="button">
+            <div className="button-text">Raphaël C.</div>
+          </button>
+        </a>
+        <button className="button">
+          <div className="button-text">About</div>
+        </button>
+        <a href="https://github.com/raphaelcolau" target="_blank" rel="noreferrer">
+          <button className="button">
+            <div className="button-text">Github</div>
+          </button>
+        </a>
+      </div>
+    )
+}
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import CanvasBackground from '../components/three.js/backgrounds.js';
 import Logo from '../components/logo/logo.js';
-
-function ButtonBar() {
-  return (
-    <div className="button-bar">
-      <a href="https://raphael.colau.fr" target="_blank" rel="noreferrer">
-        <button className="button">
-          <div className="button-text">Raphaël C.</div>
-        </button>
-      </a>
-      <button className="button">
-        <div className="button-text">About</div>
-      </button>
-      <a href="https://github.com/raphaelcolau" target="_blank" rel="noreferrer">
-        <button className="button">
-          <div className="button-text">Github</div>
-        </button>
-      </a>
-    </div>
-  )
-}
+import ButtonBar from '../components/button/ButtonBar.js';
 
 export default function PageHome() {
   return (
