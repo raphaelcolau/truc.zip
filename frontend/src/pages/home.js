b3c665b39bacbb198be2632aaf7eec0a6aa31665
Raphael C <polyster20@gmail.com>
2023-06-18 13:16:19 +0200

    Button done

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,10 +2,27 @@ import React from 'react';
 import CanvasBackground from '../components/three.js/backgrounds.js';
 import Logo from '../components/logo/logo.js';
 
+function ButtonBar() {
+  return (
+    <div className="button-bar">
+      <button className="button">
+        <div className="button-text">Raphaël C.</div>
+      </button>
+      <button className="button">
+        <div className="button-text">About</div>
+      </button>
+      <button className="button">
+        <div className="button-text">Github</div>
+      </button>
+    </div>
+  )
+}
+
 export default function PageHome() {
   return (
     <div className="page">
       <Logo />
+      <ButtonBar />
       <CanvasBackground />
     </div>
   )
