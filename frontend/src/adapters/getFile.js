edb34b523ffd6585f204e72b18304b5220937800
Raphael C <polyster20@gmail.com>
2023-06-18 14:44:12 +0200

    Create getFile.js

---
+++ frontend/src/adapters/getFile.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { apiAddress } from "../config/config";
+
+export const getFile = async (id) => {
+    const response = await axios.get(apiAddress + "/files/" + id);
+    return response.data;
+}
