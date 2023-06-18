18c63e13f7cd33f6454dff4ee4955a78d82510d3
Raphael C <polyster20@gmail.com>
2023-06-18 21:07:24 +0200

    hide overflow

--- frontend/src/components/fileInfo/FileInfo.js
+++ frontend/src/components/fileInfo/FileInfo.js
@@ -11,7 +11,7 @@ export default function FileInfo(props) {
         setInfo(data);
       });
     }, [fileId]);
-  
+
     const sizeToText = (size) => {
       if (size < 1024) {
         return size + " B";
