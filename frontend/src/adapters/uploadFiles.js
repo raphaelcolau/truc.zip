69515872f4176e625690cc0c55b6724a8492fc5b
Raphael C <polyster20@gmail.com>
2023-06-18 14:44:15 +0200

    Create uploadFiles.js

---
+++ frontend/src/adapters/uploadFiles.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { apiAddress } from "../config/config";
+
+export const uploadFiles = async (files) => {
+    const formData = new FormData();
+    formData.append("file", files);
+
+    const response = await axios.post(apiAddress + "/files/" , formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+
+    return response.data;
+};
