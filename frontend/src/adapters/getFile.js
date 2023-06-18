e12462abf503e023076aca59e7e95ec2814c7210
Raphael C <polyster20@gmail.com>
2023-06-18 15:24:38 +0200

    Fix url

--- frontend/src/adapters/getFile.js
+++ frontend/src/adapters/getFile.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { apiAddress } from "../config/config";
 
 export const getFile = async (id) => {
-    const response = await axios.get(apiAddress + "/files/" + id);
+    const response = await axios.get(apiAddress + "files/" + id);
     return response.data;
 }
--- frontend/src/adapters/uploadFiles.js
+++ frontend/src/adapters/uploadFiles.js
@@ -5,7 +5,7 @@ export const uploadFiles = async (files) => {
     const formData = new FormData();
     formData.append("file", files);
 
-    const response = await axios.post(apiAddress + "/files/" , formData, {
+    const response = await axios.post(apiAddress + "files/" , formData, {
         headers: {
             "Content-Type": "multipart/form-data",
         },
